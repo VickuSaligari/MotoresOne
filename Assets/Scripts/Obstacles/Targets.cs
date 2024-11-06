cbe6bfe25269635873790d72b6cfd10c4d3c8f7a
VickuSaligari <victoria.saligari@davinci.edu.ar>
2024-11-06 10:49:11 -0300

    Scripts and tiny scene

---
+++ Assets/Scripts/Obstacles/Targets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targets : MonoBehaviour
+{
+    /*() time limit -> 200 or more pts to win 
+     * buttons starts timer
+
+    flash 6 secs pts < 120 / 4 secs pts > 120 - cyan up / magenta down
+    if: hit -> up: 20 pts / down: 10 pts
+    color reset -> none
+
+    chance -> 1:20 when pts < 120 / 1:12 when pts > 120
+    if: hit -> (blue up / pink down): -20 pts when pts < 120 / -40 when pts > 120
+
+    chance 1:40 -> golden: +60 pts
+
+    when time´s up: =>200 pts -> door opens / <200 points: try again
+    */
+}
